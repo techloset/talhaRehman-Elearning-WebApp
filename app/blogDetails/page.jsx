f17e05ed4246880f167873acf3f51e9c0decf7a7
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2023-09-10 09:04:39 +0500

    Blog Details Page Completed

---
+++ app/blogDetails/page.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Header from '../components/header.jsx/Header'
+import Image from 'next/image'
+import blogDetailMain from '../../public/images/blogDetailsMain.svg';
+import detailsprofile from '../../public/images/blogDetailsProfile.svg';
+import ReleatedArticle from '../components/relatedArticle/ReleatedArticle';
+import Footer from '../components/footer/Footer';
+
+function page() {
+  return (
+  <>
+    {/* Header */}
+    <Header/>
+    {/* Blog Details */}
+    <div>
+        <Image src={blogDetailMain} alt="Background Image for class" />
+    </div>
+    <div className='ml-[7.25rem] mt-[5rem]'>
+        <h2 className='text-[#2F327D] text-[2.75rem] font-semibold leading-[4.95rem] '>Why Swift UI Should Be on the Radar of Every Mobile Developer</h2>
+        <p className='text-[#696984] text-[1.5rem] leading-[2.7rem] tracking-[0.03rem] max-w-[95rem]'>TOTC is a platform that allows educators to create online classes whereby they can store the course materials online; manage assignments, quizzes and exams; monitor due dates; grade results and provide students with feedback all in one place.</p>
+        <p className='text-[#696984] text-[1.5rem] leading-[2.7rem] tracking-[0.03rem] max-w-[87.5rem] mt-[1.88rem]'>TOTC is a platform that allows educators to create online classes whereby they can store the course materials online; manage assignments, quizzes and exams; monitor due dates; grade results and provide students with feedback all in one place.</p>
+        <p className='text-[#696984] text-[1.5rem] leading-[2.7rem] tracking-[0.03rem] max-w-[87.5rem]'>TOTC is a platform that allows educators to create online classes whereby they can store the course materials online; manage assignments, quizzes and exams; monitor due dates; grade results and provide students with feedback all in one place.</p>
+        <p className='text-[#696984] text-[1.5rem] leading-[2.7rem] tracking-[0.03rem] max-w-[87.5rem] mt-[1.88rem]'>TOTC is a platform that allows educators to create online classes whereby they can store the course materials online; manage assignments, quizzes and exams; monitor due dates; grade results and provide students with feedback all in one place.</p>
+        <div className='mt-[1.63rem] flex'>
+            <div className='w-[8.5rem] h-[3.0625rem] rounded-[2.25rem] bg-[rgb(73,187,189)]/[0.10] text-[#696984] text-[1rem] mr-[2.19rem] flex justify-center items-center'>affordable </div>
+            <div className='w-[8.5rem] h-[3.0625rem] rounded-[2.25rem] bg-[rgb(73,187,189)]/[0.10] text-[#696984] text-[1rem] mr-[2.19rem] flex justify-center items-center'>Stunning </div>
+            <div className='w-[8.5rem] h-[3.0625rem] rounded-[2.25rem] bg-[rgb(73,187,189)]/[0.10] text-[#696984] text-[1rem] mr-[2.19rem] flex justify-center items-center'>making </div>
+            <div className='w-[8.5rem] h-[3.0625rem] rounded-[2.25rem] bg-[rgb(73,187,189)]/[0.10] text-[#696984] text-[1rem] mr-[2.19rem] flex justify-center items-center'>madbrawns </div>
+        </div>
+        <hr className='bg-[#696984] mt-[1.94rem] max-w-[105.0625rem]' />
+        <div className='mt-[1.87rem] flex justify-between items-center mr-[8.31rem] pb-[1.87rem]'>
+            <div className='flex'>
+                <Image src={detailsprofile} alt='Profile'/>
+                <div className='flex flex-col justify-around ml-[1.25rem]'>
+                    <p className='text-[#696984] text-[0.75rem] font-medium tracking-[0.015rem]'>Written by</p>
+                    <p className='text-[#000] text-[1.125rem] font-medium tracking-[0.0225rem]'>Lina</p>
+                </div>
+            </div>
+            <div>
+                <button className='text-[#49BBBD] text-[1rem] font-bold  w-[14.5rem] h-[3.0625rem] rounded-[0.625rem] border border-[#49BBBD] bg-[rgb(73,187,189)]/[0.00]'>Follow</button>
+            </div>
+        </div>
+    </div>
+    {/* Related Blog */}
+    <ReleatedArticle />
+
+    {/* Footer */}
+    <Footer/>
+  </>
+  )
+}
+
+export default page
--- app/components/marketingArticles/Articles.jsx
+++ app/components/marketingArticles/Articles.jsx
@@ -1,7 +1,36 @@
 import Image from 'next/image'
 import React from 'react'
+import article1 from '../../../public/images/article1.svg'
+import article2 from '../../../public/images/article2.svg'
+import article3 from '../../../public/images/article3.svg'
+import article4 from '../../../public/images/article4.svg'
+import artivleblock from '../../../public/images/articleblock.svg'
+import articletime from '../../../public/images/articletime.svg'
+import profilemini from '../../../public/images/articleBlog.svg'
 
 function Articles() {
+    const marketArticle=[
+        {
+            id:1,
+            mainImg:<Image src={article1} alt='article img' className='mt-[1.25rem] ml-[1.31rem] mr-[1.13rem]'/>
+
+        },
+        {
+            id:2,
+            mainImg:<Image src={article2} alt='article img' className='mt-[1.25rem] ml-[1.31rem] mr-[1.13rem]'/>
+
+        },
+        {
+            id:3,
+            mainImg:<Image src={article3} alt='article img' className='mt-[1.25rem] ml-[1.31rem] mr-[1.13rem]'/>
+
+        },
+        {
+            id:4,
+            mainImg:<Image src={article4} alt='article img' className='mt-[1.25rem] ml-[1.31rem] mr-[1.13rem]'/>
+
+        }
+    ]
     return (
         <>
             <div>
@@ -9,22 +38,34 @@ function Articles() {
                     <h2 className='text-[#000000] text-[1.875rem] font-medium tracking-[0.0375rem]'>Marketing Articles</h2>
                     <p className='text-[#49BBBD] text-[1.25rem] font-bold'>See all</p>
                 </div>
-                <div>
-                    <div>
-                        <Image/>
-                        <div></div>
-                        <h2>AWS Certified solutions Architect</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipising elit, sed do eiusmod tempor</p>
-                        <div>
-                            <div>
-
+                <div className='ml-[7.5rem] mt-[3.13rem] pb-[10.69rem] flex gap-[3.12rem]'>
+               {marketArticle.map((info)=>(  <div className='bg-[#fff] flex flex-col articleShadow w-[23.375rem] '>
+                        {info.mainImg}
+                        <div className='flex justify-between items-center mt-[1.31rem] ml-[1.69rem]'>
+                            <div className='flex items-center'>
+                                <Image src={artivleblock} alt='block'/>
+                                <p className='text-[#696984] text-[0.875rem] font-medium leading-[1.575rem] tracking-[0.0175rem] ml-2'>Design</p>
                             </div>
-                            <div>
-                                <span></span>
-                                <h3>$80</h3>
+                            <div className='flex items-center mr-[1.19rem]'>
+                                <Image src={articletime} alt='time'/>
+                                <p className='text-[#696984] text-[0.875rem] font-medium leading-[1.575rem] tracking-[0.0175rem] ml-2'>3 Months</p>
                             </div>
                         </div>
-                    </div>
+                        <h2 className='text-[#252641] text-[1.5rem] font-medium ml-[1.69rem] mt-[1.25rem]'>AWS Certified solutions Architect</h2>
+                        <p className='ml-[1.69rem] w-[20rem] text-[#696984] text-[1.125rem] font-normal leading-[2.025rem] tracking-[0.0225rem] mt-[1.31rem]'>Lorem ipsum dolor sit amet, consectetur adipising elit, sed do eiusmod tempor</p>
+                        <div className='flex justify-between ml-[1.69rem] mt-[1.06rem] pb-[2.19rem]'>
+                                <div className='flex items-center'>
+                                    <div className='w-[2.77rem] h-[2.76559rem] rounded-full flex justify-center items-end bg-[#D9D9D9]'>
+                                    <Image src={profilemini} alt='mini profile'/>
+                                    </div>
+                                    <p className='ml-[1.11rem] text-[1.125rem] font-medium tracking-[0.0225rem]'>Lina</p>
+                                </div>    
+                            <div className='flex items-center mr-[2.37rem]'>
+                                <span className='text-[1.125rem] italic font-light tracking-[0.0225rem] line-through text-[black]/[0.50]'>$100</span>
+                                <h3 className='text-[#49BBBD] text-[1.5rem] font-bold leading-normal tracking-[0.03rem] ml-[0.88rem]'>$80</h3>
+                            </div>
+                        </div>
+                    </div>))}
                 </div>
             </div>
         </>
